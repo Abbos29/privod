3a986838258a0ad838521a6d7114d9751eae0cbe
Abbos29 <abbosvaliev2003@gmail.com>
2024-08-18 19:32:06 +0500

    final v2

--- script/script.js
+++ script/script.js
@@ -4,3 +4,26 @@ const menu = document.querySelector('.nav__menu')
 burger.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
+
+
+const overlay = document.querySelector('.overlay')
+const modalClose = document.querySelector('.modal__close')
+const modalOpenButtons = document.querySelectorAll('.modal__open') 
+
+
+modalOpenButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        overlay.classList.add('active')
+    })
+})
+
+overlay.addEventListener('click', (event) => {
+    if (event.target === overlay) {
+        overlay.classList.remove('active')
+    }
+})
+
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.remove('active')
+})
